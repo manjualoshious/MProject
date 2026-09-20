8373040000370529cd8b0d588b613a46cfec1d44
manjualoshious <manjualoshious@gmail.com>
2026-09-20 08:55:34 +0530

    Connect Streamlit to Render API

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 # Backend URL
 # =================================
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://mproject-knzg.onrender.com"
 
 
 # =================================
